709a82edd08ca35805954a7add3e3ab586fce6a5
AnjaZ <anjazubac1@gmail.com>
2020-11-23 04:49:06 -0700

    I put the code structure first

---
+++ config/config.php
@@ -0,0 +1,22 @@
+<?php
+
+define("ABSOLUTE_PATH", $_SERVER["DOCUMENT_ROOT"]."/SchoolBoardTest");
+
+define("ENV_FAJL", ABSOLUTE_PATH."/config/_env");
+
+define("SERVER", env("SERVER"));
+define("DATABASE", env("DBNAME"));
+define("USERNAME", env("USERNAME"));
+define("PASSWORD", env("PASSWORD"));
+
+function env($naziv){
+    $podaci = file(ENV_FAJL);
+    $vrednost = "";
+    foreach($podaci as $key=>$value){
+        $konfig = explode("=", $value);
+        if($konfig[0]==$naziv){
+            $vrednost = trim($konfig[1]); 
+        }
+    }
+    return $vrednost;
+}
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+  include "config/connection.php";
+
+	if(isset($_GET['student'])){
+		
+		include "models/studentResult.php";
+		
+	} 
+ ?>
+
+    
+
